65a73f5dcfa9e54e3d4d7c5f34264c7ddbb8a5d6
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2021-02-12 21:36:15 +0200

    create and add MongoseDb

--- server/server.js
+++ server/server.js
@@ -4,10 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var trip = require('../src/trips/trips.router');
+var mongoose_1 = __importDefault(require("mongoose"));
+// const trip = require('../src/trips/trips.router');
 var app = express_1.default();
 var PORT = 1717;
-app.use('/api', trip);
+var uri = 'mongodb+srv://Oliiynik_Dmt:test1@cluster0.nfppp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose_1.default.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}, function () {
+    console.log('conected to da base');
+});
+// app.use('/api',trip)
 app.listen(PORT, function () {
     console.log("Server was run http://localhost:" + PORT);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
-const trip = require('../src/trips/trips.router');
+import mongoose from 'mongoose'
+
+// const trip = require('../src/trips/trips.router');
 const app = express();
 const PORT = 1717;
+const uri ='mongodb+srv://Oliiynik_Dmt:test1@cluster0.nfppp.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
+ mongoose.connect(uri,{
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }, ()=>{
+          console.log('conected to da base');
+      });
 
-app.use('/api',trip)
+// app.use('/api',trip)
 app.listen(PORT,()=>{
     console.log(`Server was run http://localhost:${PORT}`);  
 })
